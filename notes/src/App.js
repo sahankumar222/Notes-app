9ef067c3a0dd40b1748728c1d83bc9bb0399cc96
sahankumar222 <sahankumar222@gmail.com>
2023-03-14 22:49:33 +0530

    latest

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import AppNote from './components/AppNote';
 import Header from "./components/Header"
 
 function App() {
   return (
     <div className="App">
     <Header></Header>
+    <AppNote></AppNote>
+   
  
     </div>
   );
---
+++ notes/src/components/AppNote.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function AppNote() {
+  return (
+    <div className='noteApp'>
+      <textarea placeholder='Type to add a note'> 
+        
+      </textarea>
+      <button className='dev'>save</button>
+    </div>
+  )
+}
+
+export default AppNote
