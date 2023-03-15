61d2652f755ac6f837609cfab02ac3f9bad9d700
sahankumar222 <sahankumar222@gmail.com>
2023-03-15 17:41:15 +0530

    update

--- notes/src/components/AppNote.js
+++ notes/src/components/AppNote.js
@@ -4,11 +4,20 @@ import React from 'react';
 function AppNote() {
   return (
     <div className='noteApp'>
+<div className='container'>
+
       <textarea placeholder='Type to add a note'> 
-        
+      
       </textarea>
       <button className='dev'>save</button>
+</div>
+      
     </div>
+
+ 
+  
+     
+   
   )
 }
 
